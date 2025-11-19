2f81c34180d0008c22d9bbd1aa958991f2296e85
Alicite <lunacarter1503@gmail.com>
2025-11-18 21:58:51 -0300

    rotas get e post /usuarios

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+let usuarios = []; 
+app.post('/usuarios', (req, res) => {
+    usuarios.push(req.body);
+
+    res.status(201).send('Post deu certo');
+});
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).send(usuarios); 
+});
+
+app.listen(3000);
