b92e80fbf703c592ab973523b6f5c236afc04d1b
Alicite <lunacarter1503@gmail.com>
2025-11-28 20:51:52 -0300

    adiciona os dados das requisições ao mysql

---
+++ db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+
+const conexao = async () => {
+    if (global.conexao && global.conexao.state !== 'disconnected') {
+        return global.conexao;
+    } else {
+        const db = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: '123456',
+            database: 'users'
+        });
+        global.conexao = db;
+        return db;
+    }
+}
+
+export default conexao;
--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 import express from 'express';
+import conexao from './db.js';
 
 const app = express();
 app.use(express.json());
 
-let usuarios = []; 
-app.post('/usuarios', (req, res) => {
-    usuarios.push(req.body);
+app.delete('/usuarios/:id', async (req, res) => {
+    const conec = await conexao();
+    const user = await conec.query('DELETE FROM user WHERE id = ?;', [req.params.id]);
 
-    res.status(201).send('Post deu certo');
+    res.status(204).send();
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+    const conec = await conexao();
+    const user = req.body;
+    await conec.query(
+        'UPDATE user SET nome = ?, idade = ?, email = ? WHERE id = ?;',
+        [user.nome, user.idade, user.email, req.params.id]
+    );
+
+    res.status(204).send();
+})
+
+app.post('/usuarios', async (req, res) => {
+    const conec = await conexao();
+    const user = req.body;
+    await conec.query(
+        'INSERT INTO user(nome, idade, email) VALUES(?, ?, ?);',
+        [user.nome, user.idade, user.email]
+    );
+
+    res.status(201).send(`${user.nome} foi adicionado ao BD com sucesso!`);
 });
 
-app.get('/usuarios', (req, res) => {
-    res.status(200).send(usuarios); 
+app.get('/usuarios', async (req, res) => {
+    const conec = await conexao();
+    const usuarios = await conec.query('SELECT * FROM user;');
+    res.status(200).send(usuarios[0]);
 });
 
 app.listen(3000);
